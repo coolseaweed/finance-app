a1791633eee3bfb629d901ed953226a0409f128b
coolseaweed <jeellato@gmail.com>
2024-04-15 16:08:39 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react';
+import './css/App.css';
+import './css/table.css';
+import { useState, useEffect } from 'react';
+import DataTable from "./components/dataTable";
+
+
+const headers = [
+  {
+    text: 'Name',
+    value: 'name'
+  },
+  {
+    text: 'Version',
+    value: 'version'
+  },
+  {
+    text: 'Launch Date',
+    value: 'launch'
+  }
+];
+const items = [
+  {
+    name: 'React',
+    version: '18.2.0',
+    launch: '2013-05-29'
+  },
+  {
+    name: 'Vue',
+    version: '3.2.45',
+    launch: '2014-02'
+  },
+  {
+    name: 'jQuery',
+    version: '3.3',
+    disabled: true,
+    launch: '2006-08-26'
+  },
+  {
+    name: 'Svelte',
+    version: '3.53.1',
+    launch: '2016-11-26'
+  },
+];
+
+
+
+
+
 
 function App() {
 
@@ -8,11 +53,15 @@ function App() {
   let [title, setTitle] = useState(['남자 코트 추천', '여자 코트 추천', '파이썬독학']);
   let [like, setLike] = useState(0);
 
+  const [selection, setSelection] = useState([]);
+  useEffect(() => {
+    console.log(selection);
+  }, [selection]);
+
+
   function addOne() {
     setLike(like + 1)
   }
-
-
 
   return (
     <div className="App">
@@ -29,10 +78,14 @@ function App() {
         <h4>{title[2]}</h4>
         <p>2월 17일 발행</p>
       </div>
-
-
-
-      <Modal></Modal>
+      <Modal />
+      <Modal />
+      <DataTable
+        headers={headers}
+        items={items}
+        selectable={true}
+        updateSelection={setSelection}
+      />
 
       <h4>{post}</h4>
     </div>
